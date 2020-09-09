10027eb9744757627ebf9abead1ea08a282a5d4b
xjq <lebnizjoan@gmail.com>
2020-09-10 02:06:00 +0800

    feat: update

--- 1.js
+++ 1.js
@@ -1,48 +1,59 @@
 function Node(elem, left, right) {
-  this.elem = elem
-  this.left = left || null
-  this.right = right || null
+  this.elem = elem;
+  this.left = left || null;
+  this.right = right || null;
 }
 
 function BST() {
-  this.root = null
+  this.root = null;
 }
 
 BST.prototype.insert = function (elem) {
-  const newNode = new Node(elem)
+  const newNode = new Node(elem);
   if (!this.root) {
-    this.root = newNode
+    this.root = newNode;
   } else {
-    let cur = this.root
-    let parent
+    let cur = this.root;
+    let parent;
     while (true) {
-      parent = cur
+      parent = cur;
       if (elem < cur.elem) {
-        cur = cur.left
+        cur = cur.left;
         if (cur === null) {
-          parent.left = newNode
-          break
+          parent.left = newNode;
+          break;
         }
       } else {
-        cur = cur.right
+        cur = cur.right;
         if (cur === null) {
-          parent.right = newNode
-          break
+          parent.right = newNode;
+          break;
         }
       }
     }
   }
-}
+};
 
 BST.prototype.log = function () {
-  let cur = this.root
-}
+  let cur = this.root;
+};
 
-BST.prototype.inOrder = function () {
-  let cur = this.root
-  console.log(cur.elem)
-}
+BST.prototype.find = function (elem) {
+  let cur = this.root;
+  while (cur !== null && cur.elem !== elem) {
+    if (cur.elem > elem) {
+      cur = cur.left;
+    } else {
+      cur = cur.right;
+    }
+  }
+  return cur;
+};
 
-const l = new BST()
+const l = new BST();
+l.insert(5);
+l.insert(4);
+l.insert(3);
+l.insert(1);
 
-console.log(l)
+console.log(l.find(2));
