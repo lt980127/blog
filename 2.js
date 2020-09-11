58a817f85f92e8ebf2068d869c77a8a4d4499a76
xjq <lebnizjoan@gmail.com>
2020-09-11 18:59:02 +0800

    feat: update

--- 2.js
+++ 2.js
@@ -1,64 +1,83 @@
 function Graph(v) {
-  this.vertices = v;
-  this.edges = 0;
-  this.marked = [];
+  this.vertices = v
+  this.edges = 0
+  this.marked = []
 
-  this.adj = [];
+  this.adj = []
   for (var i = 0; i < this.vertices; ++i) {
-    this.adj[i] = [];
-    this.marked[i] = false;
+    this.adj[i] = []
+    this.marked[i] = false
   }
 }
 
 Graph.prototype.addEdge = function (v, w) {
-  this.adj[v].push(w);
-  this.adj[w].push(v);
-  this.edges++;
-};
+  this.adj[v].push(w)
+  this.adj[w].push(v)
+  this.edges++
+}
 
 Graph.prototype.show = function () {
-  let log = "";
+  let log = ''
   for (var i = 0; i < this.vertices; ++i) {
-    log = `${i} -> `;
+    log = `${i} -> `
     for (var j = 0; j < this.vertices; ++j) {
-      const item = this.adj[i][j];
+      const item = this.adj[i][j]
       if (item !== undefined) {
-        log = log.concat(`${item}, `);
+        log = log.concat(`${item}, `)
       }
     }
-    console.log(log);
+    console.log(log)
   }
-};
+}
 
 Graph.prototype.bfs = function (v) {
-  const queue = [];
-  const d = [];
-  const pred = [];
+  const queue = []
+  const d = []
+  const pred = []
 
   for (let i = 0; i < this.vertices; i++) {
-    d[i] = 0;
-    pred[i] = null;
+    d[i] = 0
   }
-  queue.push(v);
-  this.marked[v] = true;
+  queue.push(v)
+  this.marked[v] = true
   while (queue.length > 0) {
-    let curV = queue.shift();
+    let curV = queue.shift()
     this.adj[curV].forEach((w) => {
       if (!this.marked[w]) {
-        d[w] = d[curV] + 1;
-        pred[w] = curV;
-        this.marked[w] = true;
-        queue.push(w);
+        d[w] = d[curV] + 1
+        const predObj = {}
+        predObj[w] = curV
+        pred.push(predObj)
+        this.marked[w] = true
+        queue.push(w)
       }
-    });
+    })
   }
-  return { d, pred };
-};
+  return { d, pred }
+}
 
-const g = new Graph(5);
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 3);
-g.addEdge(2, 4);
-g.show();
-console.log(g.bfs(0));
+const g = new Graph(5)
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 3)
+g.addEdge(2, 4)
+
+const { d, pred } = g.bfs(0)
+console.log(pred)
+
+// const start = 0
+
+// for (let i = 0; i < 4; i++) {
+//   let end = i
+//   const stack = []
+//   while (end !== start) {
+//     // console.log(pred[end])
+//     end = pred[end][1]
+//     stack.push(end)
+//   }
+//   let log = stack.pop()
+//   while (stack.length > 0) {
+//     log += ' - ' + stack.pop()
+//   }
+//   console.log(log)
+// }
